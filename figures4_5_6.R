f465db99a3898da03686be7ae9c734a2ad511fe8
Piotr Sobczyk <sobbombo@gmail.com>
2017-02-03 15:20:14 +0100

    script for creating figures 4,5,6

---
+++ figures4_5_6.R
@@ -0,0 +1,62 @@
+
+library(dplyr)
+
+source("plot_templates.R")
+
+date = "2017_02_03"
+
+scheme.names <- c("Student noise", "Additional variables", "Lognormal coefficients")
+method.names <- c("Minka BIC", "Laplace evidence", "Properly dervied BIC",
+                  "GCV", "Rajan - penalized version", "Passemier", "CSV")
+
+y <- NULL
+for(batch in c("A", "B", "C", "D", "E")){
+  f <- paste0("data/robustness_fixed_simulations", batch, "_n_100_", date, ".Rdata")
+  load(f)
+  
+  for(sim.scenario in c(1,2,3)){
+    result <- results[[sim.scenario]]
+    characteristics <- lapply(strsplit(names(result), "_"), as.numeric)
+    for(i in 1:length(result)){
+      y <- rbind(y, data.frame(pc=as.vector(result[[i]]), method=rep(method.names, numb.repetitions),
+                               snr=characteristics[[i]][1], variables=characteristics[[i]][2],
+                               scenario=scheme.names[sim.scenario]))
+    }
+  }
+}
+y$method <- factor(y$method, levels = method.names[c(1:2,4,3,5:7)])
+
+###############################################
+############## VARS PLOTS #####################
+###############################################
+
+selected.variables.number <- c(150, 800)
+selected.snrs <- c(0.25, 0.5, 0.75, 1, 2, 4)
+sim.scenario = 1
+selected.method <- method.names[c(1,3,4,7)]
+
+selected.variables.number <- c(150, 800)
+selected.snrs <- c(0.25, 0.5, 0.63, 0.75, 1, 2.00, 8)
+sim.scenario = 2
+selected.method <- method.names[c(1,3,4,7)]
+
+plot_facet_vars(y, scheme.names, sim.scenario, selected.method, selected.snrs, selected.variables.number)
+plot_name <- paste0(gsub(" ", "_", scheme.names[sim.scenario]), "_", 
+                    paste0(selected.variables.number, collapse = "_"), ".png")
+ggsave(filename = plot_name, height = 8, width = 16)
+
+
+###############################################
+############### SNR PLOTS #####################
+###############################################
+
+
+selected.variables.number <- c(50, 75, 150, 400, 800)
+selected.snrs <- c(1,4)
+sim.scenario = 1
+selected.method <- method.names[c(1,3,4,7)]
+
+plot_facet_snr(y, scheme.names, sim.scenario, selected.method, selected.snrs, selected.variables.number)
+plot_name <- paste0(gsub(" ", "_", scheme.names[sim.scenario]), "_", 
+                    paste0(selected.snrs, collapse = "_"), ".png")
+ggsave(filename = plot_name, height = 8, width = 16)
---
+++ plot_templates.R
@@ -0,0 +1,131 @@
+
+library(ggplot2)
+background="white"
+main_color="darkred"
+sobczykPlotTheme <- theme(
+  legend.position = "bottom",
+  legend.direction = "horizontal",
+  legend.box = "horizontal",
+  legend.title = element_text(face = "italic", size = 17),
+  legend.background = element_rect(fill = background),
+  legend.key = element_rect(fill = background, colour = background),
+  legend.text = element_text(size = 16),
+  plot.background = element_rect(fill = background, colour = background),
+  panel.background = element_rect(fill = background),
+  panel.background = element_rect(fill = "white"),
+  plot.title = element_text(face = "italic", size = 24, vjust = 1),
+  panel.grid.major.x = element_blank(),
+  panel.grid.major.y = element_line(),
+  panel.grid.minor.y = element_line(),
+  panel.grid.minor.x = element_line(colour = "gray95", size=1.5),
+  axis.title = element_text(size = 17),
+  axis.text = element_text(size = 15),
+  strip.text.x = element_text(size = 17, colour = "white"),
+  strip.background = element_rect(fill = main_color),
+  axis.ticks.x = element_blank()
+)
+
+#' Create a custom facet labeller function
+facet_labeller_snr <- function(variable, value) {
+  paste0("SNR=", value)
+}
+facet_labeller_vars <- function(variable, value) {
+  paste0("n=100, ", "p=", value)
+}
+
+
+plot_facet_vars <- function(y, scheme.names, sim.scenario, selected.method, selected.snrs, selected.variables.number){
+  y %>% filter(scenario==scheme.names[sim.scenario], method==selected.method[1]) %>% 
+    group_by(snr, variables, method) %>%
+    summarise(count=n()) %>% ungroup %>% select(count) %>% unlist %>% head(1) -> numb.repetitions
+  data4 <- y %>% filter(scenario==scheme.names[sim.scenario], 
+                        method %in% selected.method,
+                        variables %in% selected.variables.number,
+                        snr %in% selected.snrs) %>% 
+    mutate(snr=factor(snr)) %>%
+    group_by(snr, variables, method, pc) %>%
+    summarise(count=n())
+  y %>% filter(scenario==scheme.names[sim.scenario], 
+               method %in% selected.method,
+               variables %in% selected.variables.number,
+               snr %in% selected.snrs) %>% 
+    mutate(snr=factor(snr)) %>% group_by(snr, variables, method) %>% 
+    summarise(MeanPCs=mean(pc)) -> data5
+  
+  
+  title <- paste0(scheme.names[sim.scenario], ". Estimated number of PCs as a function of SNR.")
+  new.lables=expression(paste("PESEL", phantom()["n"]^{"hetero"}), "GCV", 
+                        paste("PESEL", phantom()["p"]^{"hetero"}), "Passemier")
+
+  
+  ggplot(data4, aes(x=snr, colour=method, group=method, shape=method)) +
+    geom_line(data=data5, aes(x=snr, y=MeanPCs)) +
+    geom_label(aes(y=pc-0.4, label=count/numb.repetitions, fill=method), color="white", position = position_dodge(width = 0.70)) +
+    geom_point(aes(y=pc,size=count/numb.repetitions), position = position_dodge(width = 0.70)) +
+    facet_grid(.~variables, labeller = facet_labeller_vars) +
+    ylab("Number of PCs") +
+    xlab("SNR") +
+    scale_y_continuous(breaks=pretty(data4$pc)) +
+    scale_size("Frequency", range = c(1,10)) +
+    scale_color_discrete("Method", labels = new.lables) +
+    scale_shape_manual("Method", values = 15:18, labels = new.lables) +
+    guides(fill=FALSE, label=FALSE,shape=guide_legend(override.aes = list(size=10, linetype=0),
+                                                      label.position = "bottom", 
+                                                      title.position = "left",
+                                                      title.hjust = 1)) +
+    guides(size=guide_legend(label.position = "bottom", title.position = "left", keyheight=4, keywidth=3)) +
+    geom_vline(xintercept=seq(1.5, length(unique(data4$snr))-0.5, 1), 
+               lwd=1, colour="gray80", linetype=2) +
+    ggtitle(title) +
+    sobczykPlotTheme
+}
+
+
+plot_facet_snr <- function(y, scheme.names, sim.scenario, selected.method, selected.snrs, selected.variables.number){
+  
+  y %>% filter(scenario==scheme.names[sim.scenario], method==selected.method[1]) %>% 
+    group_by(snr, variables, method) %>%
+    summarise(count=n()) %>% ungroup %>% select(count) %>% unlist %>% head(1) -> numb.repetitions
+  data4 <- y %>% filter(scenario==scheme.names[sim.scenario], 
+                        method %in% selected.method,
+                        variables %in% selected.variables.number,
+                        snr %in% selected.snrs) %>% 
+    mutate(snr=factor(snr)) %>%
+    group_by(snr, variables, method, pc) %>%
+    summarise(count=n())
+  y %>% filter(scenario==scheme.names[sim.scenario], 
+               method %in% selected.method,
+               variables %in% selected.variables.number,
+               snr %in% selected.snrs) %>% 
+    mutate(snr=factor(snr)) %>% group_by(snr, variables, method) %>% 
+    summarise(MeanPCs=mean(pc)) -> data5
+  
+  data4$variables <- as.factor(data4$variables)
+  data5$variables <- as.factor(data5$variables)
+  
+  title <- paste0(scheme.names[sim.scenario], ". Estimated number of PCs as a function of number of variables.")
+  new.lables=expression(paste("PESEL", phantom()["n"]^{"hetero"}), "GCV", 
+                        paste("PESEL", phantom()["p"]^{"hetero"}), "Passemier")
+
+  
+  ggplot(data4, aes(x=variables, colour=method, group=method, shape=method)) +
+    geom_line(data=data5, aes(x=variables, y=MeanPCs)) +
+    geom_label(aes(y=pc-0.4, label=count/numb.repetitions, fill=method), color="white", position = position_dodge(width = 0.70)) +
+    geom_point(aes(y=pc,size=count/numb.repetitions), position = position_dodge(width = 0.70)) +
+    facet_grid(.~snr, labeller = facet_labeller_snr) +
+    ylab("Number of PCs") +
+    xlab("Variables") +
+    scale_y_continuous(breaks=pretty(data4$pc)) +
+    scale_size("Frequency", range = c(1,10)) +
+    scale_color_discrete("Method", labels = new.lables) +
+    scale_shape_manual("Method", values = 15:18, labels = new.lables) +
+    guides(fill=FALSE, label=FALSE,shape=guide_legend(override.aes = list(size=10, linetype=0),
+                                                      label.position = "bottom", 
+                                                      title.position = "left",
+                                                      title.hjust = 1)) +
+    guides(size=guide_legend(label.position = "bottom", title.position = "left", keyheight=4, keywidth=3)) +
+    geom_vline(xintercept=seq(1.5, length(unique(data4$variables))-0.5, 1), 
+               lwd=1, colour="gray80", linetype=2) +
+    ggtitle(title) +
+    sobczykPlotTheme
+}
